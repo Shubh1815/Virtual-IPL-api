ce69bef8807d9179f051248156b203f31ba1b89b
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-10-18 22:41:47 +0530

    resolve staticfiles issue

--- virtualIPL/settings.py
+++ virtualIPL/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
 
     'rest_framework',
@@ -146,4 +145,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
