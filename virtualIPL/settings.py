5ce3b7eb3fae8ae6fd0af4d1a8d79fd7a07c6411
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-10-21 12:17:27 +0530

    config cache

--- virtualIPL/settings.py
+++ virtualIPL/settings.py
@@ -98,6 +98,26 @@ CACHES = {
         'OPTIONS': {
             'username': '6A1C07',
             'password': '31232402DDCC0189FC2ACD04E9B16443',
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
+
+                # Keep connection alive
+                'tcp_keepalive': True,
+
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
         }
     }
 }
