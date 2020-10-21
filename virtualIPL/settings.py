87e3cd58a2c0bd04c6ab007af00cdea89adea75a
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-10-21 13:34:11 +0530

    remove caching

--- virtualIPL/settings.py
+++ virtualIPL/settings.py
@@ -91,37 +91,6 @@ TEMPLATES = [
     },
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_bmemcached.memcached.BMemcached',
-        'LOCATION': 'mc2.c1.ap-northeast-1.ec2.memcachier.com',
-        'OPTIONS': {
-            'username': '6A1C07',
-            'password': '31232402DDCC0189FC2ACD04E9B16443',
-            'behaviors': {
-                # Enable faster IO
-                'no_block': True,
-                'tcp_nodelay': True,
-
-                # Keep connection alive
-                'tcp_keepalive': True,
-
-                # Timeout settings
-                'connect_timeout': 2000, # ms
-                'send_timeout': 750 * 1000, # us
-                'receive_timeout': 750 * 1000, # us
-                '_poll_timeout': 2000, # ms
-
-                # Better failover
-                'ketama': True,
-                'remove_failed': 1,
-                'retry_timeout': 2,
-                'dead_timeout': 30,
-            }
-        }
-    }
-}
-
 WSGI_APPLICATION = 'virtualIPL.wsgi.application'
 
 
